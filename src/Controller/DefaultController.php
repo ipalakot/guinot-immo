6d736bf0a11ba49242b354f8e9febe8da3a793ae
ipalakot <ipalakot@gmail.com>
2020-10-12 08:35:46 +0200

    Config routing

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="accueil")
+     * @Route("/home", name="home")
+     * @Route("/home", name="home")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
