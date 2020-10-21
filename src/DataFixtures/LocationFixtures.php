e4c7625a02deee6e6721eed5399f60e6078fa9b0
ipalakot <ipalakot@gmail.com>
2020-10-21 21:56:19 +0200

    Fixtures de Categories

--- src/DataFixtures/LocationFixtures.php
+++ src/DataFixtures/LocationFixtures.php
@@ -2,9 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Location;
+use App\Entity\Categorie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Location;
+
+
 
 class LocationFixtures extends Fixture
 {
@@ -13,26 +16,36 @@ class LocationFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-// Creer occurence de 200 
-for($i=1; $i<200; $i++){
-    $location = new Location();
-    $location->setDenomination("Denomination du Bien en Location ° $i")
-                ->setCategorie(" Appt / Maison / Villa / Hôtels... ")
-                ->setPhoto("http://placehold.it/300x200")
-                ->setCreatedAt(new \DateTime())
-                ->setDescription(" Appt / Maison / Villa / Hôtels... ")
-                ->setSurface(200)
-                ->setType(" Type du Bien ")
-                ->setChambre(4)
-                ->setEtage(2)
-                ->setPrix(200)
-                ->setAdresse(00000)
-                ->setCp(000)
-                ->setVille("Ville ")
-                ->setPays("Ville ")
-                ->setAccessibility(1);
-    $manager->persist($location);
-}
+    // Creer occurence de 10 Categroie
+    for($i=1; $i<5; $i++){
+        $categorie = new Categorie();
+        $categorie->setTitre("Categorie $i")
+                ->setResume("Resume de la categorie");
+
+        $manager->persist($categorie);
+
+// Mainteannt je cree mes Bien Locatifs
+        for($j=1; $j<10; $j++){
+            $location = new Location();
+            $location->setDenomination("Denomination du Bien en Location ° $i")
+                        ->setCategorie($categorie)
+                        ->setPhoto("http://placehold.it/300x200")
+                        ->setCreatedAt(new \DateTime())
+                        ->setDescription(" Appt / Maison / Villa / Hôtels... ")
+                        ->setSurface(200)
+                        ->setType(" Type du Bien ")
+                        ->setChambre(4)
+                        ->setEtage(2)
+                        ->setPrix(200)
+                        ->setAdresse(00000)
+                        ->setCp(000)
+                        ->setVille("Ville ")
+                        ->setPays("Ville ")
+                        ->setAccessibility(1);
+            $manager->persist($location);
+        }
+    }
+
 $manager->flush();
 }
 }
