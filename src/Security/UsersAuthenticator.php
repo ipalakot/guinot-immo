89153020899db253ed605f1a5bcf4c89ab5b2f33
ipalakot <ipalakot@gmail.com>
2020-11-12 08:24:24 +0100

    Roles?

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -97,7 +97,8 @@ class UsersAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+       // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+   
     }
 
     protected function getLoginUrl()
